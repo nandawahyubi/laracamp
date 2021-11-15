ccc09906f5b6c800d2471e4187943e7e09057884
nandawahyubi <nandawahyubi6121@gmail.com>
2021-11-15 15:06:01 +0700

    middleware dashboard, integration admin dashboard

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -26,7 +26,11 @@
             <div class="d-flex user-logged dropdown no-arrow">
                 <a href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
                     Halo, {{ Auth::user()->name }}!
+                    @if (Auth::user()->avatar)
                     <img src="{{ Auth::user()->avatar }}" class="user-photo rounded-circle" alt="">
+                    @else
+                    <img src="https://ui-avatars.com/api/?name=Admin" class="user-photo rounded-circle" alt="">
+                    @endif
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink" style="right: 0; left: auto;">
                     <li>
